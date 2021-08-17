aa0cb916e9db98750a11160871fba67b5d1a2a4c
Taylor McKay <taylormckay@gmail.com>
2021-08-16 21:20:00 -0700

    Add RSS feed for YT

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,7 +19,9 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = ()
+LINKS = (
+    ('RSS', "feeds/all.atom.xml"),
+)
 #LINKS = (
 #    ('Pelican', 'https://getpelican.com/'),
 #    ('Python.org', 'https://www.python.org/'),
@@ -30,8 +32,12 @@ LINKS = ()
 SOCIAL = ()
 #SOCIAL = (('You can add links in your config file', '#'),
 #          ('Another social link', '#'),)
-
+#
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+MENUITEMS = [
+    ['RSS', "feeds/all.atom.xml"]
+]
